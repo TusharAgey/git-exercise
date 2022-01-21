dc7326cae2d13cb36b5ee3b05b3518b3ec3709f3
ajju49 <89016384+ajju49@users.noreply.github.com>
2022-01-21 10:51:32 +0530

    adding subctraction to calc

--- calc.js
+++ calc.js
@@ -3,7 +3,7 @@ const add = (a, b) => {
 };
 
 const sub = (a, b) => {
-
+return ( a - b );
 };
 
 const mult = (a, b) => {
@@ -11,4 +11,7 @@ const mult = (a, b) => {
 };
 sum = add(4, 5);
 console.log({sum});
+
+subctraction = sub(10, 5);
+console.log({subctraction});
 
